f829f59045566cf5a13c8ba06b8b93ad5c127dd3
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-07-02 12:08:19 +0530

    Add files via upload

---
+++ program13.c
@@ -0,0 +1,38 @@
+//program to display 5 times Hello on screen
+
+#include<stdio.h>
+
+//Demonstration on ITERATION
+
+void Display()			//Defination
+{
+	int iCnt = 0;
+	
+	//1 Initilization
+	//2 Condition
+	//3 Displacement
+	//4 Loop body
+	
+//   	    1         2       3
+	for(iCnt = 1; iCnt<=5; iCnt++)
+	{
+		printf("Hello\n");		//4
+	}
+}
+									  //CMD//
+//  1    							//Hello...1
+//  2   4   3						//Hello...2
+//  2   4   3						//Hello...3
+//  2   4   3						//Hello...4
+//  2   4   3						//Hello...5
+//  2   4   3.
+//2...stop 
+
+int main()
+{
+	Display();		//Function call
+	
+	return 0;
+}
+
+
