a486a11ed8e0c2a7d3d70601fc58b0e72fbb7b84
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-07-19 00:02:09 +0530

    Add files via upload

---
+++ program82.c
@@ -0,0 +1,26 @@
+//input :5
+//output: 1  2	3	4	5
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+	int iCnt = 0;
+	
+	//     1      2           3
+	for(iCnt = 1; iCnt <= iNo; iCnt++)
+	{
+		printf("%d\t",iCnt);      //4
+	}
+}
+int main()
+{
+	int iValue = 0;
+	
+	printf("Please enter the value : \n");
+	scanf("%d",&iValue);
+	
+	Display(iValue);				//Display(5);
+	
+	return 0;
+}
---
+++ program85.c
@@ -0,0 +1,32 @@
+//input :4
+//output:1	2	3	4	4	3	2	1 
+//Complexcity = O(2N)			(time)
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+	int iCnt = 0;
+	
+	//     1      2           3
+	for(iCnt = 1; iCnt <= iNo; iCnt++)
+	{
+		printf("%d\t",iCnt);      //4
+	}
+	//     1      2           3
+	for(iCnt = iNo; iCnt >= 1; iCnt--)
+	{
+		printf("%d\t",iCnt);      //4
+	}
+}
+int main()
+{
+	int iValue = 0;
+	
+	printf("Please enter the value : \n");
+	scanf("%d",&iValue);
+	
+	Display(iValue);				//Display(5);
+	
+	return 0;
+}
