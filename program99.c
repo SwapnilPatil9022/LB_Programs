188759ac14b2b7eb73bec3f418d1569e119bf2cc
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-07-21 00:29:42 +0530

    Add files via upload

---
+++ program99.c
@@ -0,0 +1,48 @@
+//input :     Row = 4		Colume = 4
+
+//Output :  *	*	*	*
+//			*	*		*
+//			*		*	*
+//			*	*	*	*
+ 
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+	int i = 0,j = 0;
+	
+	if((iRow != iCol))
+	{
+		return;
+	}
+	
+	for(i = 1; i <= iRow; i++)
+	{
+		for(j = 1; j <= iCol; j++)
+		{
+			if((i == 1)||(i == iRow)||(j == 1)||(j == iCol)||(i == j))
+			{
+				printf("*\t");
+			}
+			else
+			{
+				printf(" \t");
+			}
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int iValue1 = 0, iValue2 =0;
+	
+	printf("Enter numebr of row : \n");
+	scanf("%d",&iValue1);
+	
+	printf("Enter number of Columne: \n");
+	scanf("%d",&iValue2);
+	
+	Display(iValue1,iValue2);				//Display(5);
+	
+	return 0;
+}
