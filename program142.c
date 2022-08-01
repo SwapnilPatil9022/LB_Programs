0cd3b3779ade7563e967e34f935391487697d4e3
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-08-01 22:44:02 +0530

    Add files via upload

---
+++ program142.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Summation(int Arr[],int iSize)
+{
+	int iSum = 0,iCnt = 0;
+	
+	for(iCnt = 0; iCnt < iSize; iCnt++)
+	{
+		iSum = iSum + Arr[iCnt];
+	}
+	return iSum;
+}
+
+int main()
+{
+	int iLength = 0;
+	int *ptr = NULL;
+	int iCnt = 0,iRet = 0;
+	
+	printf("Enter number of elements\n");
+	scanf("%d",&iLength);
+	
+	ptr = (int *)malloc(sizeof(int) *iLength);
+	
+	printf("Enter the value of array\n");
+	
+	for(iCnt = 0; iCnt < iLength; iCnt++)
+	{
+		scanf("%d",&ptr[iCnt]);
+	}
+	
+	iRet = Summation(ptr,iLength);
+	
+	printf("Addition is : %d\n",iRet);
+	
+	return 0;
+}
