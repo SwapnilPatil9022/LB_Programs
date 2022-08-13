95be41e640d2dca3c02002899d3885598055eba7
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-08-13 22:18:52 +0530

    Add files via upload

---
+++ program190.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+			// string A = string B
+bool strcmpX(char *src, char *dest)									
+{
+	 while((*src != '\0')&&(*dest != '\0'))
+	{
+        if(*src != *dest)
+		{
+			break;
+		}
+		src++;
+		dest++;
+    }
+	
+	 if(*src == '\0'&&*dest == '\0')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+	char Arr[20];		
+	char Brr[20];
+	bool bRet;
+	
+	cout<<"Enter first string"<<endl;
+	cin.getline(Arr,20);
+	
+	cout<<"Enter secound string"<<endl;
+	cin.getline(Brr,20);
+	
+	bRet = strcmpX(Arr,Brr);
+	
+	if(bRet == true)
+	{
+		cout<<"string are equal"<<endl;
+	}
+	else
+	{
+		cout<<"string are not equal"<<endl;
+	}
+
+	return 0;
+}
