4543abda55a48837069db2ee81de58e53ea37a86
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-09-04 12:42:14 +0530

    Add files via upload

---
+++ program272.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+		// Input : 6			// Output : a b c d e f
+void Display(int iNo)
+{
+	static int i = 0;
+	static char ch = 'a';
+	
+	if(i < iNo)				
+	{
+		i++;
+		Display(iNo);
+		printf("%c\t",ch);			
+		ch++;
+	}
+}
+
+int main()
+{
+	int iValue = 0;
+	
+	printf("Enter number\n");
+	scanf("%d",&iValue);
+	
+	Display(iValue);
+	return 0;
+}
