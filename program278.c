98f9292ccf31245859632859cea9c7f119848270
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-09-05 22:53:51 +0530

    Add files via upload

---
+++ program278.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+					//Reverce Recurrsion 	//Input : Hello	//Output : olleH
+void Display(char *str)
+{
+	if(*str != '\0')
+	{
+		Display(str+1);		//Head recurssion
+		printf("%c\n",*str);
+	}
+}
+
+int main()
+{
+	char Arr[20];
+	
+	printf("Enter string\n");
+	scanf("%[^'\n']s",Arr);
+	
+	Display(Arr);	// Display(920);daigram	//Call bt Address  // 
+	return 0;
+}
