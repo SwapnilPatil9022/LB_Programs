d225a8ae1273b7c368e270d6601a5366a45d5438
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-09-12 22:42:37 +0530

    Add files via upload

---
+++ program308.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+import java.util.*;
+					// User kadun string ghya ani Maximun word / size cha cha word lenth  ani world display kara.  (frequenctly ask Q)
+					//Input : India is my Country
+					//output : 7
+					//output : Country
+class program308
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter the string : ");
+		String str = sobj.nextLine();
+		
+		String newstr = str.replaceAll("\\s+"," ");		//Reguleer expression		//(rejects)
+		String newstr2 = newstr.trim();
+		String arr[] = newstr2.split(" ");
+		
+		int iPos = 0;
+		int iMax = 0;
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(arr[i].length() > iMax)
+			{
+				iMax = arr[i].length();
+				iPos = i;
+			}
+		}
+		System.out.println("Largest word  length is : "+iMax);
+		System.out.println("Largest word is : "+arr[iPos]);
+	}
+}
