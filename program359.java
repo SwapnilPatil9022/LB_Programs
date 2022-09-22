1c7dadf97346859c9b6022c6b0f4b4309599f6cf
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-09-22 11:47:27 +0530

    Add files via upload

---
+++ program359.java
@@ -0,0 +1,47 @@
+import java.lang.*;
+import java.util.*;
+			
+				//usre kadun string ghya ani tyatle frequency of each word display kra.
+class program359
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter string");
+        String str = sobj.nextLine();
+
+        String Arr[] = str.split(" ");
+
+        HashMap<String, Integer> hobj = new HashMap<>();
+        int Frequency = 0;
+
+        for(String ch : Arr)
+        {
+                if(hobj.containsKey(ch))
+                {
+                    Frequency = hobj.get(ch);
+                    hobj.put(ch, Frequency + 1);
+                }
+                else
+                {
+                    hobj.put(ch,1);
+                }
+        }
+
+        Set<String> setobj = hobj.keySet();
+        int iMax = 0;
+        String c = " ";
+
+        for(String value : setobj)
+        {
+            if(hobj.get(value) > iMax)
+            {
+                iMax = hobj.get(value);
+                c = value;
+            }
+        }
+
+        System.out.println(c +" word occurs maximum number of times ie : "+iMax);
+   }
+}
